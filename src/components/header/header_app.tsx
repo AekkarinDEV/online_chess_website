5dfbb89396fc3855519bef510e1d52cdc13f7af4
AekkarinDEV <ezio4227@gmail.com>
2025-06-14 11:42:38 +0700

    re design header

--- src/components/header/header_app.tsx
+++ src/components/header/header_app.tsx
@@ -1,20 +1,42 @@
-import { Avatar, Box, Typography } from '@mui/material'
+import { Avatar, Box, IconButton, Typography } from '@mui/material'
+import DehazeIcon from '@mui/icons-material/Dehaze'
 import styled from 'styled-components'
 
+const HeaderContainer = styled(Box)(() => ({
+  width: '100dvw',
+  height: '64px',
+  // borderBottom: '1px  solid',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  padding: '0px 16px',
+}))
+
 export const HeaderApp = () => {
-  const HeaderContainer = styled(Box)(() => ({
-    width: '100dvw',
-    height: '50px',
-    borderBottom: '1px  solid',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '0px 10px',
-  }))
   return (
     <HeaderContainer>
-      <Typography variant="h6">Middle chess</Typography>
-      <Avatar sizes="small" />
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '6px',
+        }}
+      >
+        <Avatar sizes="small" src="" variant="rounded" />
+        <Box>
+          <Typography fontSize={'18px'}>guest#3425</Typography>
+          <Typography
+            sx={{
+              fontSize: '12px',
+            }}
+          >
+            ELO: 1200
+          </Typography>
+        </Box>
+      </Box>
+      <IconButton size="large">
+        <DehazeIcon fontSize="inherit" />
+      </IconButton>
     </HeaderContainer>
   )
 }
